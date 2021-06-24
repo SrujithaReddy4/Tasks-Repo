b96be68b1c7e71425c470c41b222567a54a89892
SrujithaReddy4 <83486888+SrujithaReddy4@users.noreply.github.com>
2021-06-24 18:04:41 +0530

    Update 24thJune,2021.js

--- 24thJune,2021.js
+++ 24thJune,2021.js
@@ -35,4 +35,43 @@ class Circle {
 let c1 = new Circle();
 c1.radius = 2;
 c1.circumference();
+c1.area();
 
+//------------------------------------------------------------------------------------
+
+class Uberfare {
+    calculatefar() {
+        var Passenger_RideFare = this.BaseFare + (this.ridedistance * this.time_of_ride) + this.booking_fee;
+        console.log("Uber cab fare for the ride:" + Passenger_RideFare);
+    }
+}
+
+var uf = new Uberfare();
+uf.BaseFare = 90;
+uf.ridedistance = 50;
+uf.time_of_ride = 20;
+uf.booking_fee = 10;
+uf.calculatefar();
+
+//---------------------------------------------------------------------------
+class Person {
+    details() {
+        console.log("Name:" + this.name);
+        console.log("Age:" + this.age);
+        console.log("Proffession:" + this.proffession);
+        console.log("DOB:" + this.dob);
+        console.log("Phoneno:" + this.phoneno);
+        console.log("Email:" + this.email);
+
+    }
+
+}
+
+let c1 = new Person();
+c1.name = "Srujitha Reddy";
+c1.age = 24;
+c1.proffession = "Student";
+c1.dob = "04th-November";
+c1.phoneno = 92345678901;
+c1.email = "estybu@gdy.com"
+c1.details();
